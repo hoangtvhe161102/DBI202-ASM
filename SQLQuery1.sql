28ab35f5390bd2433161d08b1e4acd490bb55fc0
hoangtvhe161102 <107628371+hoangtvhe161102@users.noreply.github.com>
2022-07-06 14:51:59 +0700

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,19 +1,24 @@
-CREATE TABLE Student (
+CREATE TABLE Students(
 	[sid] INT NOT NULL IDENTITY(1,1),
 	[name] NVARCHAR(150) NOT NULL ,
 	[dob] DATE NOT NULL,
 	[gender] BIT NOT NULL DEFAULT 1,
 	[Address] VARCHAR(150) NOT NULL , 
-	CONSTRAINT pk_student PRIMARY KEY([sid]),											-- Rang buoc khoa chinh cho sid
-	CONSTRAINT check_name_6_to_32_character CHECK(LEN([name]) >= 6 AND LEN([name])<=32) --Rang buoc do dai cua ten hoc sinh
+	CONSTRAINT pk_student PRIMARY KEY([sid]),											
+	CONSTRAINT check_name_6_to_32_character CHECK(LEN([name]) >= 6 AND LEN([name])<=32) 
 );
+
+
+
 
 Create table Lecture(
 	[Lid] int not null Identity (1,1),
 	[name] nvarchar(150) not null,
 	[Email] nvarchar(150) not null,
-	Constraint pk_Lecture primary key([Lid]),											--Dat Lid la khoa chinh
+	Constraint pk_Lecture primary key([Lid]),										
 );
+
+
 
 Create table Subject (
 	[Suid] int not null identity (1,1),
@@ -26,7 +31,20 @@ create table Assessment (
 	[Aid] int not null identity (1,1),
 	[name] nvarchar(150) not null,
 	[Weight] decimal(4,2) not null,
-	[Suid] int not null,
-	CONSTRAINT fk_Student FOREIGN KEY (Suid) REFERENCES Student([sid]),
+	[Suid] int not null,																
+	Constraint pk_Assessment primary key([Aid]),					
 );
-drop table Assessment
+
+drop table Assessment_Students
+create table Assessment_Students(
+	[Aid] int not null,
+	[sid] int not null,
+	[Date] date not null,
+	[Score]	int not null,
+	[RetakeScore] int not null,
+	[DateRetake] date not null,
+	Constraint pk_Assessment_Student primary key(sid, Aid, [Date]),
+	CONSTRAINT pk_Students FOREIGN KEY (sid) REFERENCES Students([sid]),
+	CONSTRAINT pk_Assessment FOREIGN KEY (Aid) REFERENCES Assessment([Aid])	
+);
+ 
